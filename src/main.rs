49e1b043b0bd86d151f90ef93337730f4bbc83a9
deena_d <silja.rheingans@gmail.com>
2020-07-22 09:14:24 +0200

    added poem.txt and file reading in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+	
+	let query = &args[1];
+	let filename = &args[2];
+	
+	println!("Searching for {}", query);
+	println!("In file {}", filename);
+	
+	let mut f = File::open(filename).expect("File {}not found.", filename);
+	
+	let mut contents = String::new();
+	f.read_to_string(&mut contents).expect("File could not be read.");
+	
+	println!("With text:\n{}", contents);
 }
