b245426c93c4b797b4859e4ede537297ddfbf353
deena_d <silja.rheingans@gmail.com>
2020-07-22 10:04:40 +0200

    In main.rs: Further refactoring. Introducing error handlung with Result<T, E> in the main function as well as extracting the program logic into the run() function.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 	
-	let config = Config::new(&args);
+	let config = Config::new(&args).unwrap_or_else(|err|{
+		println!("\n\nProblem parsing arguments: {}", err);
+		process::exit(1);
+	});
 	println!("Searching for {}", config.query);
 	println!("In file {}", config.filename);
 	
-	let mut f = File::open(config.filename).expect("File not found.");
+	run(config);
+}
+
+fn run(config: Config) {
+	let mut f = File::open(config.filename)
+	    .expect("File not found.");
 	
 	let mut contents = String::new();
-	f.read_to_string(&mut contents).expect("File could not be read.");
+	f.read_to_string(&mut contents)
+	    .expect("File could not be read.");
 	
 	println!("With text:\n{}", contents);
 }
@@ -23,10 +33,16 @@ struct Config {
 }
 
 impl Config {
-	fn new(args: &[String]) -> Config {
+
+	
+	fn new(args: &[String]) -> Result<Config, &'static str> {
+		if args.len() < 3 {
+			return Err("Not enough arguments.\n\n");
+		}
+		
 		let query = args[1].clone();
 		let filename = args[2].clone();
 		
-		Config { query, filename }
+		Ok(Config { query, filename })
 	}
 }
