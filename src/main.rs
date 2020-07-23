c0249841bd36c60caa04200082cb782770e4aecd
deena_d <silja.rheingans@gmail.com>
2020-07-23 08:44:54 +0200

    Functionality moved from main.rs to lib.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,36 +12,9 @@ fn main() {
 	println!("Searching for {}", config.query);
 	println!("In file {}", config.filename);
 	
-	run(config);
-}
-
-fn run(config: Config) {
-	let mut f = File::open(config.filename)
-	    .expect("File not found.");
-	
-	let mut contents = String::new();
-	f.read_to_string(&mut contents)
-	    .expect("File could not be read.");
-	
-	println!("With text:\n{}", contents);
-}
-
-struct Config {
-	query: String,
-	filename: String,
-}
-
-impl Config {
-
-	
-	fn new(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("Not enough arguments.\n\n");
-		}
+	if let Err(e) = run(config) {
+		println!("Application error: {}", e);
 		
-		let query = args[1].clone();
-		let filename = args[2].clone();
-		
-		Ok(Config { query, filename })
+		process::exit(1);
 	}
 }
